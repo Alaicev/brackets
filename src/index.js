9952441c3c40dc91ef65fbf3ae07479df5f41637
Alaicev <altarev123456@mail.ru>
2022-02-19 14:04:41 +0400

    feat:task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false
+  let count = 0
+  str = str.split('')
+  while (count <= 40) {
+      function search() {
+          str.forEach((Item, Index)=>{
+              bracketsConfig.forEach((configItem)=>{
+                  if (Item === configItem[0]) {
+                      if (str[Index + 1] === configItem[1]) {
+                          str.splice(Index, 2)
+                      }
+                  }
+              })
+          })
+      }
+      search()
+      count++
+  }
+  return str.length <= 0;
 }
